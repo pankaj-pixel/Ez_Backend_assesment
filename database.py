0e2f21b0bc4525252b697d082daa4d02ab7e72d6
pankaj kain <75995334+pankaj-pixel@users.noreply.github.com>
2024-12-15 11:53:48 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
+# this is demo database hence credentials are exposed
 DATABASE_URL = "mysql+pymysql://root:123456@localhost/filesharedb"
 
 engine = create_engine(DATABASE_URL, echo=True)
